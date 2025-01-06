8eec20b693cea7381537723fe26dc41ff66f4f16
JoakimDahlstrom <yukimever@gmail.com>
2025-01-07 01:59:00 +0700

    fix: fix morgan config

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan(process.env.NODE_ENV === "develop" ? "dev" : "combined"));
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 
 app.use("/api", router);
 app.use(errorHandler);
